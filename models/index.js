e4265684b79abdcb5009a446f8abf811938f04cb
Federico <federico@Federicos-MacBook-Pro.local>
2019-03-09 13:47:23 -0800

    final

--- models/index.js
+++ models/index.js
@@ -8,11 +8,11 @@ var env = process.env.NODE_ENV || "development";
 var config = require(__dirname + "/../config/config.json")[env];
 var db = {};
 var mysql = require('mysql');
-var connection;
+// var connection;
 
-if(process.env.JAWSDB_URL){
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-}
+// if(process.env.JAWSDB_URL){
+//   connection = mysql.createConnection(process.env.JAWSDB_URL);
+// }
 
 
 if (config.use_env_variable) {
